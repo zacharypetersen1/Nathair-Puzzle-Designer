3784493c04eb28794684c30ebb4582b2cdd7422e
Kevin Vylet <kevylet@ucsc.edu>
2016-03-15 21:27:10 -0700

    successfully finds (maybe working) paths in coordinate form

--- graph.py
+++ graph.py
@@ -23,92 +23,103 @@ class Graph:
         """
 
         # direction is right
-        if direction == 0:         
-            if rules[location[0] + 1, location[1]] == 'Wall' or location[0] + 1 > width:
+        if direction == 0:
+            if location[0] + 1 >= self.width:
                 return None
-            elif rules[location[0] + 1, location[1]] == 'None' or rules[location[0] + 1, location[1]] == 'Open':
+            elif self.rules[location[0] + 1][location[1]] == 'Wall':
+                return None
+            elif self.rules[location[0] + 1][location[1]] == 'None' or self.rules[location[0] + 1][location[1]] == 'Open' or self.rules[location[0] + 1][location[1]] == 'Start':
                 return [location[0] + 1, location[1]]
             else:
-                teleporter = rules[location[0] + 1, location[1]] # save the tp #
-                for x, column in enumerate(rules): # iterate through the rules to find the tp
+                teleporter = self.rules[location[0] + 1][location[1]] # save the tp #
+                for x, column in enumerate(self.rules): # iterate through the self.rules to find the tp
                     for y, value in enumerate(column):
-                        if value == teleporter and (x != location[0] or y != location[1]):
-                            return getAdjacent([x, y], 0) # retry the check at the new position
-                        else: # there is no matching teleporter
-                            return None
+                        if value == teleporter and (x != location[0] + 1 or y != location[1]):
+                            return self.getAdjacent([x, y], 0) # retry the check at the new position
+                return None # there is no matching teleporter
         # direction is down
-        elif direction == 2:    
-            if rules[location[0], location[1] + 1] == 'Wall':
+        elif direction == 2:
+            if location[1] + 1 >= self.height:
                 return None
-            elif rules[location[0], location[1] + 1] == 'None' or rules[location[0], location[1] + 1] == 'Open':
+            elif self.rules[location[0]][location[1] + 1] == 'Wall':
+                return None
+            elif self.rules[location[0]][location[1] + 1] == 'None' or self.rules[location[0]][location[1] + 1] == 'Open' or self.rules[location[0]][location[1] + 1] == 'Start':
                 return [location[0], location[1] + 1]
             else:
-                teleporter = rules[location[0], location[1] + 1] # save the tp #
-                for x, column in enumerate(rules): # iterate through the rules to find the tp
+                teleporter = self.rules[location[0]][location[1] + 1] # save the tp #
+                for x, column in enumerate(self.rules): # iterate through the self.rules to find the tp
                     for y, value in enumerate(column):
-                        if value == teleporter and (x != location[0] or y != location[1]):
-                            return getAdjacent([x, y], 2) # retry the check at the new position
-                        else: # there is no matching teleporter
-                            return None
+                        if value == teleporter and (x != location[0] or y != location[1] + 1):
+                            return self.getAdjacent([x, y], 2) # retry the check at the new position
+                return None # there is no matching teleporter
          # direction is left
-        elif direction == 4:   
-            if rules[location[0] - 1, location[1]] == 'Wall':
+        elif direction == 4:
+            if location[0] - 1 < 0:
                 return None
-            elif rules[location[0] - 1, location[1]] == 'None' or rules[location[0] - 1, location[1]] == 'Open':
+            elif self.rules[location[0] - 1][location[1]] == 'Wall':
+                return None
+            elif self.rules[location[0] - 1][location[1]] == 'None' or self.rules[location[0] - 1][location[1]] == 'Open' or self.rules[location[0] - 1][location[1]] == 'Start':
                 return [location[0] - 1, location[1]]
             else:
-                teleporter = rules[location[0] - 1, location[1]] # save the tp #
-                for x, column in enumerate(rules): # iterate through the rules to find the tp
+                teleporter = self.rules[location[0] - 1][location[1]] # save the tp #
+                for x, column in enumerate(self.rules): # iterate through the self.rules to find the tp
                     for y, value in enumerate(column):
-                        if value == teleporter and (x != location[0] or y != location[1]):
-                            return getAdjacent([x, y], 4) # retry the check at the new position
-                        else: # there is no matching teleporter
-                            return None
+                        if value == teleporter and (x != location[0] - 1 or y != location[1]):
+                            return self.getAdjacent([x, y], 4) # retry the check at the new position
+                return None # there is no matching teleporter
         # direction is up
-        else:                       
-            if rules[location[0], location[1] - 1] == 'Wall':
+        else:
+            if location[1] - 1 < 0:
                 return None
-            elif rules[location[0], location[1] - 1] == 'None' or rules[location[0], location[1] - 1] == 'Open':
+            elif self.rules[location[0]][location[1] - 1] == 'Wall':
+                return None
+            elif self.rules[location[0]][location[1] - 1] == 'None' or self.rules[location[0]][location[1] - 1] == 'Open'  or self.rules[location[0]][location[1] - 1] == 'Start':
                 return [location[0], location[1] - 1]
             else:
-                teleporter = rules[location[0], location[1] - 1] # save the tp #
-                for x, column in enumerate(rules): # iterate through the rules to find the tp
+                teleporter = self.rules[location[0]][location[1] - 1] # save the tp #
+                for x, column in enumerate(self.rules): # iterate through the self.rules to find the tp
                     for y, value in enumerate(column):
-                        if value == teleporter and (x != location[0] or y != location[1]):
-                            return getAdjacent([x, y], 6) # retry the check at the new position
-                        else: # there is no matching teleporter
-                            return None
+                        if value == teleporter and (x != location[0] or y != location[1] - 1):
+                            return self.getAdjacent([x, y], 6) # retry the check at the new position
+                return None # there is no matching teleporter
 
     def convertToDir(self, list):
         # Takes in a list of coordinates and returns a list of directions that links them
+        #for i, coord in enumerate(list):
         return None
 
 
     def findPathsDFS(self):
         # Returns all valid paths through the defined graph
         # Returns:
-        #    A list of paths in the form of a list of coordinates
+        #    A list of paths in the form of a list of directions
     
-        path = []
-        stack = [(0, start)]
-        path_list = [(0, start)]
-        parent = {}
-        while queue:
-            while parent[stack[len(stack) - 1]] == path[len(path) - 1]:     # if the next position is adjacent to the head 
+        path = [self.start]
+        stack = [(self.start, None)]
+        path_list = []
+        while stack:
+            while stack[-1][1] == path[-1] or len(path) == 1:         # if the next position is adjacent to the head
                 v = stack.pop()     # get the next snake position
-                path.append(v)      # append it to the path
+                if len(path) != 1 or v[0] != self.start:
+                    path.append(v[0])      # append it to the path
                 i = 6
-                while i <= 0:       # get and the next three adjacent positions
-                    adj = getAdjacent(v, i)
-                    if (adj and adj not in path) or adj == start:
-                        stack.append(adj)   # if not walls or OOB add them to the stack
-                        parent[adj] = v     # give adjacent values parents in the dict
+                while i >= 0:       # get and the next three adjacent positions
+                    adj = [self.getAdjacent(v[0], i), None]
+                    if adj[0] and (adj[0] not in path or (adj[0] == self.start and len(path) > 1)):
+                        adj[1] = v[0]          # store parent
+                        stack.append(adj)      # if not walls or OOB add them to the stack
+                    elif adj[0] == self.start:
+                        print('shrek')
                     i -= 2
-            if(path[len(path) - 1] == start)
+                if len(stack) == 0:
+                    break
+            #print(path[-1])
+            if path[-1] == self.start:
                 path_list.append(path)                                      # if not and the head is the start save the list
-            path.pop()                                                      # pop off the head of the path
-        for p in path_list:
-            p = convertToDir(p)
+            if path:
+                path.pop()                                                      # pop off the head of the path
+        #for p in path_list:
+            #p = convertToDir(p)
         return path_list
 
 
@@ -127,3 +138,9 @@ sample_rules = [
 ]
 
 sample_graph = Graph([4, 5], sample_rules, [3, 0])
+
+#adjacent = sample_graph.getAdjacent([1,4],6)
+#print(adjacent)
+
+#paths = sample_graph.findPathsDFS()
+#print(paths)
